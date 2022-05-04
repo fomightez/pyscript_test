562082b5b8fd35fdb62e77ca882608615bdb7876
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2022-05-04 13:36:19 -0400

    make name reflect the notebook used by default only has markdown cells

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function getUrl() {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
-    return params['url'] == null? './test_nb.ipynb' : params['url']
+    return params['url'] == null? './test_nb_md_only.ipynb' : params['url']
 }
 
 window.onload = function() { 
