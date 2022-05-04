7376aaea0068258c8b059b11737cd431e836fc68
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2022-05-04 13:49:15 -0400

    make a version of index.js just for the markdown only tests

---
+++ index_4_md_only.js
@@ -0,0 +1,9 @@
+function getUrl() {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    return params['url'] == null? './test_nb_md_only.ipynb' : params['url']
+}
+
+window.onload = function() { 
+    window.nblite.openNbAsPyScript(getUrl(), 'nb-content')
+};
