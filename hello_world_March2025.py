92e35a25aa5baaf381cb4faa39edc490e05ff0a7
Wayne's Bioinformatics Code Portal <fomightez@gmail.com>
2025-03-17 16:58:52 -0400

    Update hello_world_March2025.py

--- hello_world_March2025.py
+++ hello_world_March2025.py
@@ -1,5 +1,6 @@
 from pyscript import display
 from datetime import datetime
 now = datetime.now()
-s = 'Hello world!\n\nThis is the current date and time, as computed by Python:\n'
-display(s,now.strftime("%m/%d/%Y, %H:%M:%S"))
+s1 = "Hello world!"
+s2 = "This is the current date and time, as computed by Python:"
+display(s1,s2,now.strftime("%m/%d/%Y, %H:%M:%S"))
